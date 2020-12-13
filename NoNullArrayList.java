99303ab63d42b08f2da577508a19f43ddbddcc76
Shriya <>
2020-12-13 17:44:56 -0500

    starting capacity done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,5 +3,14 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(){
     super();
   }
-  
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+
+  public static void main(String[] args){
+    //Holder<Integer> data = new Holder<Integer>();
+    NoNullArrayList<Integer> test = new NoNullArrayList<Integer>();
+    System.out.println();
+
+  }
 }
