4ad2bc4f35f2359373a9192b03e1df4b147c92cb
Shriya <>
2020-12-13 18:34:17 -0500

    set done

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,5 +5,8 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
-
+  public set(int index, T value){
+    super.remove(index);
+    super.add(index, value);
+  }
 }
