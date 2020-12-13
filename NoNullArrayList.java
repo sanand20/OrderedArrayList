20bf9b0a422af5159b42267ca0ba80313fe2c48f
Shriya <>
2020-12-13 17:55:55 -0500

    set done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,6 +7,13 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super(startingCapacity);
   }
 
+  public T set (int index, T value){
+    if (value == null){
+      throw new IllegalArgumentException("error");
+    }
+    return super.set(index, value);
+}
+
   public static void main(String[] args){
     //Holder<Integer> data = new Holder<Integer>();
     NoNullArrayList<Integer> test = new NoNullArrayList<Integer>();
