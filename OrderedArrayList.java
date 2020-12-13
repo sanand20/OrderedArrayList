5e18fb2aee9d419774fd2f177fef5175e1dac4e6
Shriya <>
2020-12-13 18:58:16 -0500

    helper method added

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,10 +5,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
+
+  public int findIndex(T newVal){
+    for (int i = 0; i < size(); i++) {
+      if (newVal.compareTo(this.get(i)) <= 0) {
+          return i;
+      }
+    }
+    return size();
+  }
+
+
+
+
+
   public T set(int index, T value){
     T ans = get(index);
     super.remove(index);
     super.add(index, value);
     return ans;
   }
+
+
+
 }
